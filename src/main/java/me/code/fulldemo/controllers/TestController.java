8427e23ca6ea4216b4a3e434a30dd8831ec1779f
Haris Avdicevic <haris.avdi93@gmail.com>
2022-12-26 13:40:22 +0100

    Just to be safe

---
+++ src/main/java/me/code/fulldemo/controllers/TestController.java
@@ -0,0 +1,15 @@
+package me.code.fulldemo.controllers;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class TestController {
+
+    @GetMapping("/test")
+    public String test() {
+        System.out.println("Called!");
+        return "This test controller is working!";
+    }
+
+}
--- src/main/java/me/code/fulldemo/security/SecurityConfig.java
+++ src/main/java/me/code/fulldemo/security/SecurityConfig.java
@@ -1,21 +1,13 @@
 package me.code.fulldemo.security;
 
 import me.code.fulldemo.services.UserService;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
 import org.springframework.security.authentication.AuthenticationManager;
-import org.springframework.security.authentication.AuthenticationProvider;
-import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
-import org.springframework.security.config.annotation.ObjectPostProcessor;
-import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.config.http.SessionCreationPolicy;
-import org.springframework.security.core.userdetails.UserDetailsService;
-import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.CorsConfigurationSource;
@@ -40,6 +32,8 @@ public class SecurityConfig {
                 .addFilter(new JWTLoginFilter(authManager))
                 .addFilterAfter(new JWTVerifyFilter(userService), JWTLoginFilter.class)
                 .authorizeRequests()
+                .antMatchers("/test")
+                .permitAll()
                 .antMatchers("/user/register")
                 .permitAll()
                 .antMatchers(HttpMethod.PUT, "/product/")
